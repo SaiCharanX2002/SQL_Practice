153b8dce4e5a3659f0aef8f31bd2033a51ae8957
Sai Charan Bola <bolachharan1@gmail.com>
2025-12-05 13:01:41 +0530

    Add files via upload

---
+++ 3rd_Qn.sql
@@ -0,0 +1,18 @@
+CREATE TABLE employees (
+    emp_id INT PRIMARY KEY,
+    emp_name VARCHAR(50),
+    dept_name VARCHAR(50),
+    salary INT
+);
+
+
+INSERT INTO employees (emp_id, emp_name, dept_name, salary) VALUES
+(101, 'Charan', 'HR', 30000),
+(102, 'Priya', 'HR', 35000),
+(103, 'Nithesh', 'IT', 45000),
+(104, 'Sai', 'IT', 47000),
+(105, 'Reddy', 'Finance', 42000)
+
+SELECT *
+FROM employees
+WHERE salary = (SELECT MAX(salary) FROM employees);
